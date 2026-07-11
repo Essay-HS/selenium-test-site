efc67b8e9a90179d79742001d231f2a931858de0
A C <ac@As-Mac-mini.lan>
2026-07-11 01:58:07 -0500

    Allow tests to run against local or live environments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+import os
+
 import pytest
 from selenium import webdriver
 
 
 @pytest.fixture
 def base_url():
-    return "http://127.0.0.1:5000"
+    return os.getenv(
+        "BASE_URL",
+        "http://127.0.0.1:5000",
+    )
 
 
 @pytest.fixture
